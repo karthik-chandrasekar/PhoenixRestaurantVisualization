76f6f87f8578454fd64197b18bc71d202c10bd8a
george <georgethomas.mec@gmail.com>
2015-04-18 12:02:35 -0700

    adding old colors

--- senti.js
+++ senti.js
@@ -15,8 +15,8 @@ function loadColorMap() {
     var colorObj = {};
     var prevMap = null;
     var mapper = d3.scale.quantize();
-    var colors     = d3.scale.category20b().range();
-//    var colors     = ["blue", "green","yellow", "orange", "red"];
+//    var colors     = d3.scale.category20b().range();
+    var colors     = ["blue", "green","yellow", "orange", "red"];
     mapper.range(colors);
 
     colorObj.setMin = function(minArg) {
